487ed04821df3d7bf147ef2e135b05ef03580953
Manuel <manuelgrom@users.noreply.github.com>
2017-08-22 20:16:14 +0200

    added basic min max calculation
    math shit

--- NumberWizard/Assets/NumberWizard.cs
+++ NumberWizard/Assets/NumberWizard.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+    private float _guess = 0.0f;
     private int _maxNumber = 1000;
     private int _minNumber = 1;
     private float _rndNumber = 1.0f;
 
+    private void Awake()
+    {
+        _rndNumber = Random.Range(_minNumber, _maxNumber);
+        _guess = _rndNumber;
+    }
+
     // Use this for initialization
     void Start()
     {
-        _rndNumber = Random.Range(_minNumber, _maxNumber);
-
         print("Think of a number");
-
         print("The maximum number can be " + _maxNumber);
         print("The minimum number can be " + _minNumber);
-
-        print("is your number " + _rndNumber + " ?");
+        print("is your number " + _guess + " ?");
         print("up key for higher, down key for lower");
     }
 
@@ -27,11 +30,18 @@ public class NumberWizard : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            print("Down arrow was pressed");
+            //print("Down arrow was pressed");
+            _maxNumber = (int)_guess;
+            _guess = (_maxNumber + _minNumber) / 2;
+            print("Higher or Lower then " + _guess);
+
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            print("Up arrow was pressed");
+            //print("Up arrow was pressed");
+            _minNumber = (int)_guess;
+            _guess = (_maxNumber + _minNumber) / 2;
+            print("Higher or Lower then " + _guess);
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
